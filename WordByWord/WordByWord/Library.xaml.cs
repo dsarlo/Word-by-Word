0e79144fca7b4511b551f9211321490374ab445c
dsarlo <danielsrl9@gmail.com>
2018-09-21 18:35:31 -0400

    Added necessary nuget packages and initial library/reader screens

---
+++ WordByWord/WordByWord/Library.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WordByWord
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Library : Window
+    {
+        public Library()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WordByWord/WordByWord/Reader.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WordByWord
+{
+    /// <summary>
+    /// Interaction logic for Reader.xaml
+    /// </summary>
+    public partial class Reader : Window
+    {
+        public Reader()
+        {
+            InitializeComponent();
+        }
+    }
+}
