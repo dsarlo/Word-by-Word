0ca61a38438f0fcda2297cf0719f4be72e1faec6
dsarlo <danielsrl9@gmail.com>
2018-09-22 00:09:59 -0400

    Finished up the UI and added remaining nuget packages

--- WordByWord/WordByWord/Library.xaml.cs
+++ WordByWord/WordByWord/Library.xaml.cs
@@ -1,15 +1,27 @@
-﻿using System.Windows;
+﻿using System.Windows.Controls;
+using MahApps.Metro.Controls;
 
 namespace WordByWord
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Library : Window
+    public partial class Library : MetroWindow
     {
+        private readonly ViewModel.ViewModel _viewModel;
+
         public Library()
         {
             InitializeComponent();
+
+            _viewModel = new ViewModel.ViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.Hide();
+            new Reader().Show();
         }
     }
 }
--- WordByWord/WordByWord/ViewModel/ViewModel.cs
+++ WordByWord/WordByWord/ViewModel/ViewModel.cs
@@ -1,7 +1,37 @@
-﻿
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Command;
+
 namespace WordByWord.ViewModel
 {
-    public class ViewModel
+    public class ViewModel : ObservableObject
     {
+        private ObservableCollection<string> _library = new ObservableCollection<string>();
+
+        public ViewModel()
+        {
+            AddDocumentCommand = new RelayCommand(AddDocument);
+        }
+
+        #region Properties
+
+        public RelayCommand AddDocumentCommand { get; }
+
+        public ObservableCollection<string> Library
+        {
+            get => _library;
+            set { Set(() => Library, ref _library, value); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void AddDocument()
+        {
+            _library.Add("HELLO");
+        }
+
+        #endregion
     }
 }
