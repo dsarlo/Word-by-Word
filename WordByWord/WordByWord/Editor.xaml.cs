36dceeef8afbcdab4ea673e65f6d277a2dbd374d
daniel goings <dcgoings@buffalo.edu>
2018-09-22 01:31:07 -0400

    Initial Editor Screen
    Associated with Task #28 - Create Editor/Expansion screen. Added textbox and confirm button.

---
+++ WordByWord/WordByWord/Editor.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace WordByWord
+{
+    /// <summary>
+    /// Interaction logic for Editor.xaml
+    /// </summary>
+    public partial class Editor : MetroWindow
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void editorConfirm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
